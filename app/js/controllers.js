101829446cd0018783eec99da585b585ff9a2912
Natalia Fierro <nataliabfierro@gmail.com>
2013-08-28 09:34:49 -0700

    adds basic functionality to chat, updates css and img folder

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,29 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('wradio.controllers', []).
+  controller('ChatCtrl', [
+  	'$scope', 
+  	'angularFire',
+  	function($scope, angularFire ) {
+  		var url = 'https://wradio.firebaseio.com/chat';
+  		var promise = angularFire(url, $scope, 'chat', {messages: []});
 
-  }])
-  .controller('MyCtrl2', [function() {
+  // 	$scope.msgInput = "Type here";
+  // 	$scope.name = "Tom";
+  // };
 
-  }]);
+  	// callback
+  	promise.then(function(){
+  
+  		$scope.AddMsg = function(){
+  		$scope.chat.messages.push({text: $scope.msgInput})
+  		}
+
+		
+  	});
+  }
+
+]);
+
+ 

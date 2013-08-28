40adf14211bd9473d1d6dc659f7ef9b69e9d1e3d
Natalia Fierro <nataliabfierro@gmail.com>
2013-08-28 15:49:24 -0700

    adds guest and temporary username

--- js/controllers.js
+++ js/controllers.js
@@ -3,28 +3,27 @@
 /* Controllers */
 
 angular.module('wradio.controllers', []).
-  controller('ChatCtrl', [
+ controller('ChatCtrl', [
   	'$scope', 
   	'angularFire',
   	function($scope, angularFire ) {
   		var url = 'https://wradio.firebaseio.com/chat';
   		var promise = angularFire(url, $scope, 'chat', {messages: []});
 
-  // 	$scope.msgInput = "Type here";
-  // 	$scope.name = "Tom";
-  // };
+  		$scope.username = 'Guest' + Math.floor(Math.random()*101);
+
+ 
 
   	// callback
-  	promise.then(function(){
-  
-  		$scope.AddMsg = function(){
-  		$scope.chat.messages.push({text: $scope.msgInput})
-  		}
+  	// promise has to do with the asyncronizity of firebase
+	  	promise.then(function(){
+	  
+	  		$scope.AddMsg = function(){
+	  		$scope.chat.messages.push({text: $scope.msgInput, from: $scope.username})
+	  		}
+  		});
+   }
 
-		
-  	});
-  }
-
-]);
+ ]);
 
  

118e2e02ea45c7cba127c321bfcdea86881f9997
Natalia Fierro <nataliabfierro@gmail.com>
2013-09-04 17:17:37 -0700

    routing for partials added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,14 @@ angular.module('wradio',
 	[
 		'firebase',
 		'wradio.controllers'
-	]
-);
+	]).
+	config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+			when('/home', {templateUrl: 'partials/home.html'}).
+			when('/about', {templateUrl: 'partials/about.html'}).
+			when('/contact', {templateUrl: 'partials/contact.html'}).
+			when('/stuff', {templateUrl: 'partials/stuff.html'}).
+			otherwise({redirectTo: '/home'});
+	}]);
  
 
